ab3bfbf9df392e1343145c76a5ed9a17f6c04050
GhostKato <osadchenko.maksym@gmail.com>
2025-10-31 09:34:41 +0200

    add main logo

---
+++ src/components/BigLogo.vue
@@ -0,0 +1,15 @@
+<template>
+  <img :src="logo" alt="Логотип" class="logo" />
+</template>
+
+<script setup lang="ts">
+import logo from '@/assets/images/logo.png'
+</script>
+
+<style scoped>
+.logo {
+  width: 100%;
+  max-width: 250px;
+  height: auto;
+}
+</style>
---
+++ src/components/MainHeader.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="container">
+    <BigLogo />
+  </div>
+</template>
+
+<script setup lang="ts">
+import BigLogo from '@/components/BigLogo.vue'
+</script>
+
+<style>
+.container {
+  background-color: #000000;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100px;
+}
+</style>
