25400924afdae250d1db469011360d04a6ab5401
GhostKato <osadchenko.maksym@gmail.com>
2025-11-03 12:03:13 +0200

    fixed scrollbar

--- src/App.vue
+++ src/App.vue
@@ -15,8 +15,33 @@ body {
   margin: 0;
   padding: 0;
   box-sizing: border-box;
-  overflow-x: hidden;
   height: 100%;
   color: white;
+  background-color: #000;
+}
+body {
+  overflow-y: scroll;
+  overflow-x: hidden;
+}
+html {
+  overflow: hidden;
+}
+body::-webkit-scrollbar {
+  width: 8px;
+}
+body::-webkit-scrollbar-track {
+  background: #1a1a1a;
+}
+body::-webkit-scrollbar-thumb {
+  background-color: #000;
+  border-radius: 4px;
+  border: 1px solid #1a1a1a;
+}
+body::-webkit-scrollbar-thumb:hover {
+  background-color: #4d4d4d;
+}
+body {
+  scrollbar-width: thin;
+  scrollbar-color: #333 #1a1a1a;
 }
 </style>
